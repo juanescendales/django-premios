3b8ec260f717d46d8bdeea1e926f3df52b368fda
Juan Cendales <juanescendales@gmail.com>
2023-01-25 11:08:36 -0500

    feat: models creation

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,3 +1,18 @@
-from django.db import models  # noqa: F401
+from django.db import models
 
-# Create your models here.
+
+# Class Question
+class Question(models.Model):
+    question_text = models.CharField(max_length=200, verbose_name="question's text")
+    published_date = models.DateTimeField(
+        auto_now_add=True, verbose_name="date published"
+    )
+
+
+# Class Choice
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="question_choices"
+    )
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
